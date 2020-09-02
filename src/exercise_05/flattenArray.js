3af0ed6175a7335a29ae4effe7af0590fbac0929
wudibin <781721794@qq.com>
2020-09-02 21:47:20 +0800

    Finish flattenArray.js exercise

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,20 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array == undefined) {
+      throw new Error('Flatten undefined or null array');
+  } else if (!array.length) {
+      return array;
+  }
+  let arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] instanceof Array) {
+      for (let j = 0; j < array[i].length; j++) {
+        arr.push(array[i][j]);
+      }
+    } else {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
 }
