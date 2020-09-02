1e3e1b7f7966b100e70c017a1db06ba162dede4c
wudibin <781721794@qq.com>
2020-09-02 16:54:06 +0800

    Finish characterCounter.js exercise

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,17 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (string == undefined || !string) {
+      return 0;
+  }
+  if (prediction == undefined) {
+    return string.length;
+  }
+  for (let i = 0; i < string.length; i++) {
+    if (prediction(string.charAt(i))) {
+      const regex = new RegExp(string.charAt(i), 'g');
+      const result = string.match(regex);
+      return !result ? 0 : result.length;
+    }
+  }
 }
